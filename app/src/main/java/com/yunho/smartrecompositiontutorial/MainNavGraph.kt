f13fc584f1a5a01bdb25a06678748d34bfa57ea7
lyh990517 <lyh990517@naver.com>
2025-08-25 17:10:45 +0900

    optimize

--- app/src/main/java/com/yunho/smartrecompositiontutorial/MainNavGraph.kt
+++ app/src/main/java/com/yunho/smartrecompositiontutorial/MainNavGraph.kt
@@ -6,18 +6,18 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
+import com.yunho.smartrecompositiontutorial.cases.advenced.classStabilityInference
+import com.yunho.smartrecompositiontutorial.cases.advenced.contentType
+import com.yunho.smartrecompositiontutorial.cases.advenced.phaseSkipping
 import com.yunho.smartrecompositiontutorial.cases.advenced.stateCalculationWithDerivedStateOf
 import com.yunho.smartrecompositiontutorial.cases.advenced.stateCalculationWithSnapshotFlow
+import com.yunho.smartrecompositiontutorial.cases.advenced.stateDelegation
+import com.yunho.smartrecompositiontutorial.cases.advenced.stateDelegationUsingLambda
+import com.yunho.smartrecompositiontutorial.cases.advenced.stateHoisting
 import com.yunho.smartrecompositiontutorial.cases.basic.donutHoleSkipping
 import com.yunho.smartrecompositiontutorial.cases.basic.optimizedCalculation
 import com.yunho.smartrecompositiontutorial.cases.basic.positionalMemoization
 import com.yunho.smartrecompositiontutorial.cases.basic.stateLoop
-import com.yunho.smartrecompositiontutorial.cases.advenced.stateDelegation
-import com.yunho.smartrecompositiontutorial.cases.advenced.stateDelegationUsingLambda
-import com.yunho.smartrecompositiontutorial.cases.advenced.phaseSkipping
-import com.yunho.smartrecompositiontutorial.cases.advenced.stateHoisting
-import com.yunho.smartrecompositiontutorial.cases.advenced.classStabilityInference
-import com.yunho.smartrecompositiontutorial.cases.advenced.contentType
 
 @Composable
 fun MainNavGraph(
--- app/src/main/java/com/yunho/smartrecompositiontutorial/cases/advenced/ContentType.kt
+++ app/src/main/java/com/yunho/smartrecompositiontutorial/cases/advenced/ContentType.kt
@@ -7,14 +7,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
